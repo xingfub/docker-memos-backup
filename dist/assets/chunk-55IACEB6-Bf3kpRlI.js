d821043575340fb8497b2eb1624f37b145ecb379
ubuntu <ubuntu@localhost.localdomain>
2025-10-20 11:42:38 +0800

    1

---
+++ dist/assets/chunk-55IACEB6-Bf3kpRlI.js
@@ -0,0 +1,1 @@
+import{_ as a,e}from"./mermaid-vendor-Be1fRuBG.js";var g=a((o,t)=>{let n;return t==="sandbox"&&(n=e("#i"+o)),(t==="sandbox"?e(n.nodes()[0].contentDocument.body):e("body")).select(`[id="${o}"]`)},"getDiagramElement");export{g};
--- start.sh
+++ start.sh
@@ -10,14 +10,11 @@ script_dir=$(dirname "$script_path")
 
 memosName="memos"
 memosPort=5230
-mortisName="mortis"
-mortisPort=5231
+
 
 sudo docker stop $memosName
 sudo docker rm $memosName
 
-sudo docker stop $mortisName
-sudo docker rm $mortisName
 
 sudo docker run -d \
   --name $memosName \
@@ -26,12 +23,4 @@ sudo docker run -d \
   -v $script_dir/.memos/:/var/opt/memos \
   neosmemo/memos:0.25.1
 
-sudo docker run -d \
-  --name $mortisName \
-  -p $mortisPort:5231 \
-  --link memos:$memosName \
-  --entrypoint "/app/mortis" \
-  ghcr.io/mudkipme/mortis:0.25.1 \
-  -grpc-addr=172.17.0.4:5230 -addr=0.0.0.0 -port=5231
-  
 
