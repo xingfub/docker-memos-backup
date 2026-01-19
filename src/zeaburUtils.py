defd2c665d8b756169a3c86aea383d3b57092e47
Your Name <you@example.com>
2026-01-19 12:45:06 +0800

    1

--- src/zeaburUtils.py
+++ src/zeaburUtils.py
@@ -6,13 +6,17 @@ import os
 
 apiToken="sk-mjs5s7ldwoqpe5kgsimp5onipx5ko"
 
+# 读取工作流级别的环境变量
+serviceId = os.environ.get('SERVICEID', '696db2e980817a3f9d9ad5de')
+print(f"环境变量 SERVICEID: {serviceId}")
+
 def downFile():
     localDbFile="memos_prod.db"
     if os.path.exists(localDbFile):
         os.remove(localDbFile)
     path="/var/opt/memos/memos_prod.db"
     req = requests.get(
-        f"https://api.zeabur.com/projects/69532a06faf9a4fc1e128a72/services/696da00a80817a3f9d9acf34/files?path={path}&environment=69532a063c33a611f1237ab8",
+        f"https://api.zeabur.com/projects/69532a06faf9a4fc1e128a72/services/{serviceId}/files?path={path}&environment=69532a063c33a611f1237ab8",
         headers={"Authorization": f"Bearer {apiToken}"}
     )
     # 保存到本地
