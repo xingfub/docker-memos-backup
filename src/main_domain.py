85a5c0a9ba5e912f3868769201ac252897bae696
Your Name <you@example.com>
2026-02-09 14:30:26 +0800

    1

--- src/main_domain.py
+++ src/main_domain.py
@@ -1,7 +1,3 @@
-#  任务名称
-#  name: script name
-#  定时规则
-#  cron: 1 9 * * *
 import requests
 import json
 import datetime
@@ -19,7 +15,7 @@ def send_post_json_request(domain_name,expiry_date,remaining_days):
                 "password": "xin",
                 "recipient": "proud2008@qq.com",
                 "subject": "域名到期提醒",
-                "message": f"域名{domain_name}到期日期为{expiry_date}，距离到期还有{remaining_days}天",
+                "content": f"域名{domain_name},到期日期为{expiry_date}，距离到期还有{remaining_days}天",
             }
         
         # 发送请求
@@ -37,7 +33,20 @@ def get_domain_expire_date(domain_name):
         # 构建URL
         url = f"https://dash.domain.digitalplat.org/whois?name={domain_name}"
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 QuarkPC/6.3.5.725'
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 QuarkPC/6.3.5.725',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'zh-CN,zh;q=0.9',  
+            'Connection': 'keep-alive',
+            'cache-control': 'max-age=0',
+            'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
         }
         # 发送GET请求
         response = requests.get(url, timeout=60, headers=headers)
@@ -79,12 +88,16 @@ if __name__ == "__main__":
     domain_name='xingfub.dpdns.org'
     expire_date = get_domain_expire_date(domain_name)
     if expire_date is None:
-        print( f"无法获取域名{domain_name}到期日期,程序退出")
-        exit(1)
+        print( f"无法通过接口获取域名{domain_name}到期日期")
+        expire_date =os.environ.get("XINGFUB_DOMAIN_EXPIRE")
+        if expire_date is None:
+            print( f"无法通过环境变量获取域名{domain_name}到期日期")
+            print("程序退出")
+            exit(1)
     print(f"域名 {domain_name} 的到期日期是: {expire_date}")
     days_until = calculate_days_until(expire_date)
     print(f"距离 {expire_date} 还有 {days_until} 天")
-    if days_until > 90:
+    if days_until > 290:
         print("不需要发送邮件提醒,程序退出")
         exit(1)
     t=send_post_json_request(domain_name,expire_date,days_until)
